8683944a05abc63a5e155ea6fad4a95df3c82f82
Takuya Matsumoto <takumats@yahoo-corp.jp>
2019-03-01 15:24:47 +0900

    add

--- config/athenz_config_test.go
+++ config/athenz_config_test.go
@@ -1,189 +1,107 @@
 package config
 
 import (
-	"context"
-	"net/http"
-	"net/http/httptest"
-	"reflect"
-	"regexp"
+	"fmt"
+	//	"context"
+	//	"net/http"
+	//	"net/http/httptest"
+	//	"reflect"
+	//	"regexp"
 	"testing"
-	"time"
-
-	ntokend "github.com/yahoojapan/athenz-ntokend"
-	"github.com/kpango/gache"
+	"sync"
+	//	"time"
+	//	ntokend "github.com/yahoojapan/athenz-ntokend"
 )
 
 func TestNewAthenzConfd(t *testing.T) {
 	type args struct {
-		host string
-		key  string
-		c    gache.Gache
-	}
-	type test struct {
-		name string
-		args args
-		want AthenzConfd
-	}
-	tests := []test{
-		func() test {
-			ga := gache.New().StartExpired(context.Background(), time.Second)
-			return test{
-				name: "",
-				args: args{
-					host: "test.domain.com",
-					key:  "aaa",
-					c:    ga,
-				},
-				want: &confd{
-					sysAuthDomain: "sys.auth",
-					etagExpTime:   12 * time.Hour,
-					etagCache:     ga,
-				},
-			}
-		}(),
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := NewAthenzConfd(tt.args.host, tt.args.key, tt.args.c); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("NewAthenzConfd() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_confd_FetchAthenzConfig(t *testing.T) {
-	type fields struct {
-		athenzHost    string
-		sysAuthDomain string
-		authHeaderKey string
-		ntoken        ntokend.TokenProvider
-		client        *http.Client
-		etagCache     gache.Gache
-		etagExpTime   time.Duration
-	}
-	type args struct {
-		ctx context.Context
-	}
-	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		want    *AthenzConfig
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := &confd{
-				athenzHost:    tt.fields.athenzHost,
-				sysAuthDomain: tt.fields.sysAuthDomain,
-				authHeaderKey: tt.fields.authHeaderKey,
-				ntoken:        tt.fields.ntoken,
-				client:        tt.fields.client,
-				etagCache:     tt.fields.etagCache,
-				etagExpTime:   tt.fields.etagExpTime,
-			}
-			got, err := c.FetchAthenzConfig(tt.args.ctx)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("confd.FetchAthenzConfig() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("confd.FetchAthenzConfig() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
-func Test_confd_fetchPubKeyEntries(t *testing.T) {
-	type fields struct {
-		athenzHost    string
-		sysAuthDomain string
-		authHeaderKey string
-		ntoken        ntokend.TokenProvider
-		client        *http.Client
-		etagCache     gache.Gache
-		etagExpTime   time.Duration
-	}
-	type args struct {
-		ctx context.Context
-		env athenzEnv
+		opts []Option
 	}
 	type test struct {
 		name      string
-		fields    fields
 		args      args
-		want      *sysAuthConfig
-		afterFunc func()
-		wantErr   bool
+		checkFunc func(AthenzConfd) error
 	}
-	regex = regexp.MustCompile("^(http|https)://")
 	tests := []test{
-		func() test {
-			handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-				w.Write([]byte(`{"name":"yby.wdiu.travel.travel-site","publicKeys":[{"key":"testkey","id":"0"}],"modified":"2018-05-07T06:29:11.163Z"}%`))
-				w.WriteHeader(200)
-			})
-			srv := httptest.NewTLSServer(handler)
-
-			return test{
-				name: "get pub key entries successful",
-				fields: fields{
-					athenzHost:    regex.ReplaceAllString(srv.URL, ""),
-					sysAuthDomain: "auth.domain",
-					authHeaderKey: "",
-					ntoken: func() (string, error) {
-						return "", nil
-					},
-					client:      srv.Client(),
-					etagCache:   gache.New().StartExpired(context.Background(), time.Second),
-					etagExpTime: time.Second,
+		test{
+			name: "new athenz confd success",
+			args: args{
+				opts: []Option{},
+			},
+			checkFunc: func(got AthenzConfd) error {
+				if got.(*confd).sysAuthDomain != "sys.auth" {
+					return fmt.Errorf("cannot set default options")
+				}
+				return nil
+			},
+		},
+		{
+			name: "new athenz confd success with options",
+			args: args{
+				opts: []Option{
+					SysAuthDomain("dummyd"),
+					AthenzURL("dummyURL"),
 				},
-				args: args{
-					ctx: context.Background(),
-					env: "travel-site",
-				},
-				want: &sysAuthConfig{
-					Modified: "2018-05-07T06:29:11.163Z",
-					Name:     "yby.wdiu.travel.travel-site",
-					PublicKeys: []publicKey{
-						{
-							ID:  "0",
-							Key: "testkey",
-						},
-					},
-				},
-				afterFunc: func() {
-					srv.Close()
-				},
-				wantErr: false,
-			}
-		}(),
+			},
+			checkFunc: func(got AthenzConfd) error {
+				if got.(*confd).sysAuthDomain != "dummyd" || got.(*confd).athenzURL != "dummyURL" {
+					return fmt.Errorf("cannot set optional params")
+				}
+				return nil
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if tt.afterFunc != nil {
-				defer tt.afterFunc()
+			got, err := NewAthenzConfd(tt.args.opts...)
+			if err != nil {
+				t.Errorf("NewAthenzConfd() =  %v", err)
 			}
-
-			c := &confd{
-				athenzHost:    tt.fields.athenzHost,
-				sysAuthDomain: tt.fields.sysAuthDomain,
-				authHeaderKey: tt.fields.authHeaderKey,
-				ntoken:        tt.fields.ntoken,
-				client:        tt.fields.client,
-				etagCache:     tt.fields.etagCache,
-				etagExpTime:   tt.fields.etagExpTime,
-			}
-			got, err := c.fetchPubKeyEntries(tt.args.ctx, tt.args.env)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("confd.fetchPubKeyEntries() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("confd.fetchPubKeyEntries() = %v, want %v", got, tt.want)
+			err = tt.checkFunc(got)
+			if err != nil {
+				t.Errorf("NewAthenzConfd() = %v", err)
 			}
 		})
 	}
 }
+
+//func Test_GetPubKeu(t *testing.T) {
+//	c := &confd{
+//		confCache: &AthenzConfig{
+//			ZMSPubKeys: new(sync.Map),
+//			ZTSPubKeys: new(sync.Map),
+//		},
+//	}
+//	zmsVer := &VerifierMock{}
+//	ztsVer := &VerifierMock{}
+//	c.confCache.ZMSPubKeys.Store("0",zmsVer)
+//	c.confCache.ZTSPubKeys.Store("0",ztsVer)
+//	type args struct {
+//		env AthenzEnv
+//		keyID string
+//	}
+//	type test struct {
+//		name string
+//		args args
+//		want VerifierMock
+//	}
+//	tests := []test{
+//		test{
+//			name: "success",
+//			args: args{
+//				env: "zms",
+//				keyID: "0",
+//			},
+//			want: zmsVer,
+//		},
+//	}
+//	for _, tt := range tests {
+//		t.Run(tt.name, func(t *testing.T) {
+//			got, err := c.getPubKey(tt.args.env, tt.args.keyID)
+//			if err != nil {
+//				t.Errorf("getPubKey() =  %v", err)
+//			}
+//		})
+//	}
+//
+//}
--- config/option_test.go
+++ config/option_test.go
@@ -2,7 +2,6 @@ package config
 
 import (
 	"fmt"
-	"net/http"
 	"testing"
 	"time"
 )
