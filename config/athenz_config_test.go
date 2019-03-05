79bf5a41cc1d55bd6bc0621cf668d4cafde15f0f
Takuya Matsumoto <takumats@yahoo-corp.jp>
2019-03-05 09:58:04 +0900

    fix

--- config/athenz_config_test.go
+++ config/athenz_config_test.go
@@ -1,14 +1,23 @@
 package config
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"time"
 	//	"context"
 	//	"net/http"
 	//	"net/http/httptest"
 	//	"reflect"
 	//	"regexp"
-	"testing"
 	"sync"
+	"testing"
+
+	"github.com/kpango/gache"
+	"github.com/pkg/errors"
+	cmp "github.com/google/go-cmp/cmp"
 	//	"time"
 	//	ntokend "github.com/yahoojapan/athenz-ntokend"
 )
@@ -77,7 +86,7 @@ func Test_GetPubKey(t *testing.T) {
 	c.confCache.ZMSPubKeys.Store("0", zmsVer)
 	c.confCache.ZTSPubKeys.Store("0", ztsVer)
 	type args struct {
-		env AthenzEnv
+		env   AthenzEnv
 		keyID string
 	}
 	type test struct {
@@ -89,34 +98,137 @@ func Test_GetPubKey(t *testing.T) {
 		test{
 			name: "get success",
 			args: args{
-				env: "zts",
+				env:   "zts",
 				keyID: "0",
 			},
 			want: ztsVer,
 		},
-		test{
-			name: "not found",
-			args: args{
-				env: "zms",
-				keyID: "1",
-			},
-			want: nil,
-		},
+		//		test{
+		//			name: "not found",
+		//			args: args{
+		//				env: "zms",
+		//				keyID: "1",
+		//			},
+		//			want: nil,
+		//		},
 		{
 			name: "invalid env",
 			args: args{
-				env: "dummy",
+				env:   "dummy",
 				keyID: "0",
 			},
 			want: zmsVer,
 		},
-
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := c.getPubKey(tt.args.env, tt.args.keyID)
 			if got != tt.want {
 				t.Errorf("getPubKey() = expect: %v	result: %v", tt.want, got)
+			}
+		})
+	}
+}
+
+func Test_confign_fetchPubKeyEntries(t *testing.T) {
+	type fields struct {
+		refreshDuration  time.Duration
+		errRetryInterval time.Duration
+		etagCache        gache.Gache
+		etagFlushDur     time.Duration
+		etagExpTime      time.Duration
+		athenzURL        string
+		sysAuthDomain    string
+		client           *http.Client
+		confCache        *AthenzConfig
+	}
+	type args struct {
+		ctx context.Context
+		env AthenzEnv
+	}
+	type test struct {
+		name      string
+		fields    fields
+		args      args
+		checkFunc func(c *confd, sac *SysAuthConfig, upd bool, err error) error
+	}
+	tests := []test{
+		func() test {
+			handler := http.HandlerFunc(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Add("ETag", "dummyEtag")
+				w.Write([]byte(`{"name":"dummyDom.zms","publicKeys":[{"key":"dummyKey","id":"dummyID"}],"modified":"2017-01-23T02:20:09.331Z"}`))
+				w.WriteHeader(http.StatusOK)
+			}))
+			srv := httptest.NewTLSServer(handler)
+
+			return test{
+				name: "test fetch success",
+				fields: fields{
+					athenzURL:     strings.Replace(srv.URL, "https://", "", 1),
+					sysAuthDomain: "dummyDom",
+					etagCache:     gache.New(),
+					etagExpTime:   time.Minute,
+					client:        srv.Client(),
+					confCache: &AthenzConfig{
+						ZMSPubKeys: new(sync.Map),
+						ZTSPubKeys: new(sync.Map),
+					},
+				},
+				args: args{
+					ctx: context.Background(),
+					env: "zms",
+				},
+				checkFunc: func(c *confd, sac *SysAuthConfig, upd bool, err error) error {
+					if err != nil {
+						return err
+					}
+
+					_, ok := c.etagCache.Get("dummyDomain")
+					if ok {
+						return errors.New("invalid etag was set")
+					}
+
+					want := &SysAuthConfig{
+						Modified: "2017-01-23T02:20:09.331Z",
+						Name:     "dummyDom.zms",
+						PublicKeys: []*PublicKey{
+							{
+								ID:  "dummyID",
+								Key: "dummyKey",
+							},
+						},
+					}
+
+					if !cmp.Equal(sac, want) {
+						return errors.Errorf("not match, got: %v, want: %v", sac, want)
+					}
+
+					if upd == false {
+						return errors.New("Invalid upd flag")
+					}
+
+					return err
+				},
+			}
+		}(),
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := &confd{
+				refreshDuration:  tt.fields.refreshDuration,
+				errRetryInterval: tt.fields.errRetryInterval,
+				etagCache:        tt.fields.etagCache,
+				etagFlushDur:     tt.fields.etagFlushDur,
+				etagExpTime:      tt.fields.etagExpTime,
+				athenzURL:        tt.fields.athenzURL,
+				sysAuthDomain:    tt.fields.sysAuthDomain,
+				client:           tt.fields.client,
+				confCache:        tt.fields.confCache,
+			}
+			got, got1, err := c.fetchPubKeyEntries(tt.args.ctx, tt.args.env)
+
+			if err := tt.checkFunc(c, got, got1, err); err != nil {
+				t.Errorf("c.fetchPubKeyEntries() error = %v", err)
 			}
 		})
 	}
