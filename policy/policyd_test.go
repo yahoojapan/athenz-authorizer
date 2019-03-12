4c0bdddb71e0bca339cbfd046549ae48937b59dd
Takuya Matsumoto <takumats@yahoo-corp.jp>
2019-03-12 16:50:57 +0900

    fix

--- policy/policyd_test.go
+++ policy/policyd_test.go
@@ -198,6 +198,7 @@ func Test_policy_StartPolicyUpdator(t *testing.T) {
 				checkFunc: func(p *policy, ch <-chan error) error {
 					time.Sleep(time.Millisecond*100)
 					cancel()
+					time.Sleep(time.Millisecond *50)
 					asss, ok := p.rolePolicies.Get("dummyDom:role.dummyRole")
 					if !ok {
 						return errors.New("rolePolicies is empty")
