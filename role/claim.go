78ff228889fd9068a2d77afece625766f5049227
Kyo Fujisaki <kyfujisa@yahoo-corp.jp>
2020-04-17 20:10:35 +0900

    Delete access token code from role package

--- role/claim.go
+++ role/claim.go
@@ -17,43 +17,8 @@ limitations under the License.
 package role
 
 import (
-	"fmt"
-	"time"
-
-	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/yahoojapan/athenz-authorizer/v2/access"
 )
-
-type BaseClaim struct {
-	jwt.StandardClaims
-}
-
-// Valid is copy from source code, and changed c.VerifyExpiresAt parameter.
-func (c *BaseClaim) Valid() error {
-	vErr := new(jwt.ValidationError)
-	now := jwt.TimeFunc().Unix()
-
-	if !c.VerifyExpiresAt(now, true) {
-		delta := time.Unix(now, 0).Sub(time.Unix(c.ExpiresAt, 0))
-		vErr.Inner = fmt.Errorf("token is expired by %v", delta)
-		vErr.Errors |= jwt.ValidationErrorExpired
-	}
-
-	if !c.VerifyIssuedAt(now, false) {
-		vErr.Inner = fmt.Errorf("Token used before issued")
-		vErr.Errors |= jwt.ValidationErrorIssuedAt
-	}
-
-	if !c.VerifyNotBefore(now, false) {
-		vErr.Inner = fmt.Errorf("token is not valid yet")
-		vErr.Errors |= jwt.ValidationErrorNotValidYet
-	}
-
-	if vErr.Errors == 0 {
-		return nil
-	}
-
-	return vErr
-}
 
 // RoleJWTClaim represents role jwt claim data.
 type RoleJWTClaim struct {
@@ -66,18 +31,5 @@ type RoleJWTClaim struct {
 	UserID   string `json:"u"`
 	UserName string `json:"n"`
 	Version  string `json:"v"`
-	BaseClaim
-}
-
-// OAuth2AccessTokenClaim represents access token claim data.
-// based on https://github.com/yahoo/athenz/blob/0e7335dbfa9d41eef0b049c07e7f846bff0f3169/libs/java/auth_core/src/main/java/com/yahoo/athenz/auth/token/AccessToken.java#L382
-type OAuth2AccessTokenClaim struct {
-	AuthTime       int64             `json:"auth_time"`
-	Version        int               `json:"ver"`
-	ClientID       string            `json:"client_id"`
-	UserID         string            `json:"uid"`
-	ProxyPrincipal string            `json:"proxy,omitempty"`
-	Scope          []string          `json:"scp"`
-	Confirm        map[string]string `json:"cnf"`
-	BaseClaim
+	access.BaseClaim
 }
