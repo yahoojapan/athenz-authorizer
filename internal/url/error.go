a96d060c601369f0f3e9f543119d39f19f9466d3
Kyo Fujisaki <kyfujisa@yahoo-corp.jp>
2020-09-11 18:35:29 +0900

    Fix error message

--- internal/url/error.go
+++ internal/url/error.go
@@ -4,8 +4,8 @@ import (
 	"errors"
 )
 
-// ErrEmptyAthenzJwksURL is "Athenz URL is empty"
-var ErrEmptyAthenzJwksURL = errors.New("Athenz JKU is empty")
+// ErrEmptyAthenzJwksURL is "Athenz JWK Set URL is empty"
+var ErrEmptyAthenzJwksURL = errors.New("Athenz JWK Set URL is empty")
 
 // ErrUnsupportedScheme is "Unsupported scheme, only support HTTP or HTTPS"
 var ErrUnsupportedScheme = errors.New("Unsupported scheme, only support HTTP or HTTPS")
